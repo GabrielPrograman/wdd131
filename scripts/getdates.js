c0bab8fafbdf413361bec0e24fc143b29bdf3a0b
GabrielPrograman <gabrielalejandro0843@gmail.com>
2026-05-16 19:12:47 -0400

    first page done v3

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,9 @@
 const yearSpan = document.querySelector("#currentyear");
-yearSpan.textContent = new Date().getFullYear();
+if (currentYearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+}
 
 const lastModified = document.querySelector("#lastmodified");
-lastModified.textContent = `Last Modified: ${document.lastModified}`;
+if (lastModifiedParagraph) {
+    lastModified.textContent = `Last Modified: ${document.lastModified}`;
+}
